ccb3d7e3f7bd010c44f1bdf7e803662e5ec90427
Filipe Silva <filipeqs@outlook.com>
2021-07-31 07:57:55 -0400

    setup repository and dependency injection

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,4 +1,6 @@
+using leave_management.Contracts;
 using leave_management.Data;
+using leave_management.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +32,12 @@ namespace leave_management
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add references for Repository and Contracts
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveHistoryRepository, LeaveHistoryRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
