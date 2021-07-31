6721d757bb440d460c497a3c7f1d6396c6cc5304
Filipe Silva <filipeqs@outlook.com>
2021-07-31 11:09:07 -0400

    leavetype controller

---
+++ leave-management/Controllers/LeaveTypeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using leave_management.Contracts;
+using leave_management.Data;
+using leave_management.Models;
+using leave_management.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeRepository _repo;
+        private readonly IMapper _mapper;
+
+        public LeaveTypesController(ILeaveTypeRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        // GET: LeaveTypes
+        public IActionResult Index()
+        {
+            var leaveTypes = _repo.FindAll().ToList();
+            var model = _mapper.Map<List<LeaveType>, List<DetailsLeaveTypeVM>>(leaveTypes);
+
+            return View(model);
+        }
+    }
+}
