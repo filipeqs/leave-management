931bcd944c68c3aa5c051a9e56e696db21c12a9c
Filipe Silva <filipeqs@outlook.com>
2021-08-01 10:35:19 -0400

    seed database with admin and roles

---
+++ leave-management/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace leave_management
+{
+    public static class SeedData
+    {
+        public static async Task Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await SeedRoles(roleManager);
+            await SeedUsers (userManager);
+        }
+
+        private static async Task SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            var adminUser = await userManager.FindByNameAsync("admin");
+            if (adminUser == null)
+            {
+                var user = new IdentityUser { UserName = "admin@email.com", Email = "admin@email.com" };
+                var result = await userManager.CreateAsync(user, "P@ssword1");
+
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(user, "Administrator");
+            }
+        }
+        
+        private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            var adminRoleExists = await roleManager.RoleExistsAsync("Administrator");
+            if (!adminRoleExists)
+            {
+                var role = new IdentityRole { Name = "Administrator" };
+                await roleManager.CreateAsync(role);
+            }
+
+            var employeeRoleExists = await roleManager.RoleExistsAsync("Employee");
+            if (!employeeRoleExists)
+            {
+                var role = new IdentityRole { Name = "Employee" };
+                await roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
